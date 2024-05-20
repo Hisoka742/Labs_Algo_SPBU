a010b19a8bb4065050ef6c19a9a25ba8343c5daa
SALMANE_ATMANIA <113948229+Hisoka742@users.noreply.github.com>
2024-05-20 16:19:43 +0300

    Add files via upload
    Реализация класса ones_compl_int для целочисленной арифметики в обратном коде с параметром шаблона BASE может выглядеть следующим образом:
    
    Определим основные операции:
    
    Конструкторы и операторы присваивания.
    Преобразование между прямым и обратным кодом.
    Арифметические операции.
    Операторы сравнения.
    Ввод и вывод.
    Учтем знаковость:
    
    Для хранения значения используем BASE, который представляет собой целочисленный тип (например, int32_t).
    Реализуем арифметические операции:
    
    Сложение, вычитание, умножение и деление в обратном коде.

---
+++ Нестандартная арифметика/olgoci_1.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+#include <limits>
+#include <type_traits>
+
+template<typename BASE>
+class ones_compl_int {
+    static_assert(std::is_integral<BASE>::value, "BASE must be an integral type");
+
+    BASE value;
+
+    // Преобразование числа в обратный код
+    BASE toOnesComplement(BASE val) const {
+        return ~val;
+    }
+
+    // Преобразование числа из обратного кода в прямой
+    BASE fromOnesComplement(BASE val) const {
+        return ~val;
+    }
+
+public:
+    // Конструкторы
+    ones_compl_int() : value(0) {}
+    ones_compl_int(BASE val) : value(toOnesComplement(val)) {}
+
+    // Оператор присваивания
+    ones_compl_int& operator=(BASE val) {
+        value = toOnesComplement(val);
+        return *this;
+    }
+
+    // Получение значения в прямом коде
+    BASE get() const {
+        return fromOnesComplement(value);
+    }
+
+    // Арифметические операторы
+    ones_compl_int operator+(const ones_compl_int& other) const {
+        return ones_compl_int(fromOnesComplement(value) + fromOnesComplement(other.value));
+    }
+
+    ones_compl_int operator-(const ones_compl_int& other) const {
+        return ones_compl_int(fromOnesComplement(value) - fromOnesComplement(other.value));
+    }
+
+    ones_compl_int operator*(const ones_compl_int& other) const {
+        return ones_compl_int(fromOnesComplement(value) * fromOnesComplement(other.value));
+    }
+
+    ones_compl_int operator/(const ones_compl_int& other) const {
+        return ones_compl_int(fromOnesComplement(value) / fromOnesComplement(other.value));
+    }
+
+    // Операторы сравнения
+    bool operator==(const ones_compl_int& other) const {
+        return value == other.value;
+    }
+
+    bool operator!=(const ones_compl_int& other) const {
+        return value != other.value;
+    }
+
+    bool operator<(const ones_compl_int& other) const {
+        return fromOnesComplement(value) < fromOnesComplement(other.value);
+    }
+
+    bool operator<=(const ones_compl_int& other) const {
+        return fromOnesComplement(value) <= fromOnesComplement(other.value);
+    }
+
+    bool operator>(const ones_compl_int& other) const {
+        return fromOnesComplement(value) > fromOnesComplement(other.value);
+    }
+
+    bool operator>=(const ones_compl_int& other) const {
+        return fromOnesComplement(value) >= fromOnesComplement(other.value);
+    }
+
+    // Ввод и вывод
+    friend std::ostream& operator<<(std::ostream& os, const ones_compl_int& num) {
+        os << num.get();
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, ones_compl_int& num) {
+        BASE val;
+        is >> val;
+        num = ones_compl_int(val);
+        return is;
+    }
+};
+
+// Пример использования
+int main() {
+    ones_compl_int<int32_t> a, b;
+
+    std::cout << "Input For  a: ";
+    std::cin >> a;
+
+    std::cout << "Input For  b: ";
+    std::cin >> b;
+
+    std::cout << "a: " << a << "\n";
+    std::cout << "b: " << b << "\n";
+
+    ones_compl_int<int32_t> c = a + b;
+    std::cout << "a + b: " << c << "\n";
+
+    return 0;
+}
+
